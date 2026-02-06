1c614898614447b42c860adaff2e9f91e9b4e737
IsaacRWI <isaacngrwi@gmail.com>
2026-02-06 11:05:21 +0000

    Added neighbors - Changed dead cells to show how many alive cells are in the 8 cells around it

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import itertools
+from collections import Counter
+
 alive = "🥳"
 dead = "😞"
 width = 5
 height = 5
 board ={(2, 1), (2, 2), (2, 3)}
+neighbors = list(itertools.product([0, 1, -1], repeat=2))[1:]
+count = Counter((x + dx, y + dy) for x, y in board for dx, dy in neighbors)
 
 def row(y):
-    return (alive if (x, y) in board else dead for x in range(width))
+    return (alive if (x, y) in board else str(count[(x,y)]) for x in range(width))
 
 def draw(board):
     for y in range(height):
