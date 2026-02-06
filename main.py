267c2ae2049b993ba7a4f2b6f85f3fc8f32fdaf8
IsaacRWI <isaacngrwi@gmail.com>
2026-02-06 11:30:41 +0000

    Fixed code to work properly in cmd

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import itertools
 from collections import Counter
+import os
+import time
 
 alive = "🥳"
-dead = "😞"
+dead = "👿"
 width = 5
 height = 5
 board ={(2, 1), (2, 2), (2, 3)}
@@ -10,7 +12,7 @@ neighbors = list(itertools.product([0, 1, -1], repeat=2))[1:]
 counter = Counter((x + dx, y + dy) for x, y in board for dx, dy in neighbors)
 
 def row(y):
-    return (alive if (x, y) in board else str(counter[(x,y)]) for x in range(width))
+    return (alive if (x, y) in board else dead for x in range(width))
 
 def draw(board):
     for y in range(height):
@@ -25,9 +27,11 @@ def update(board):
 
 def play(board, n=5):
     for i in range(n):
+        os.system("cls")
         draw(board)
         print("---")
         board = update(board)
+        time.sleep(0.5)
 
 
 # draw(board)
